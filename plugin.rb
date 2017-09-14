f6b5a9915c2f12e28fd660669e3b683dbd0fd2bd
Tobias Nordahl Kristensen <tobiasnordahl@gmail.com>
2017-09-14 18:09:52 +0200

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,8 +1,8 @@
-# name: layouts-custom-html
+# name: layouts-discordinfo
 # about: A custom html widget for use with Discourse Layouts
 # version: 0.1
 # authors: Angus McLeod
 
 after_initialize do
-  DiscourseLayouts::WidgetHelper.add_widget('custom-html')
+  DiscourseLayouts::WidgetHelper.add_widget('discordinfo')
 end
